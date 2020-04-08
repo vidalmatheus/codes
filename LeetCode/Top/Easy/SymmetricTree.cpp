21625959b3b211ed4940e16c83aa35e2d5c8683e
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-04-08 02:01:50 -0300

    SymmetricTree

--- LeetCode/Top/Easy/SymmetricTree.cpp
+++ LeetCode/Top/Easy/SymmetricTree.cpp
@@ -73,9 +73,38 @@ private:
 
 };
 
+class IterativeSolution {
+public:
+    bool isSymmetric(TreeNode* root){
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* node1 = q.front();
+            q.pop();
+            TreeNode* node2 = q.front();
+            q.pop();
+            if (!node1 && !node2)
+                continue;
+            if (!node1 ^ !node2)
+                return false;
+            if (node1->val!=node2->val)
+                return false;
+            q.push(node1->left);
+            q.push(node2->right);
+            q.push(node1->right);
+            q.push(node2->left);
+        }
+
+        return true;
+    }
+};
+// Time: O(n)
+// Space: O(n)
+
 int main(){ 
     // Symmetric Tree
-/*     TreeNode *root = new TreeNode(1);
+    TreeNode *root = new TreeNode(1);
     root->left = new TreeNode(2);
     root->right = new TreeNode(2);
     
@@ -83,21 +112,24 @@ int main(){
     root->right->right = new TreeNode(3);
 
     root->left->right = new TreeNode(4);
-    root->right->left = new TreeNode(4); */
+    root->right->left = new TreeNode(4);
 
     // Not a Symmetric Tree
-    TreeNode *root = new TreeNode(1);
-    root->left = new TreeNode(2);
-    root->right = new TreeNode(2);
+    // TreeNode *root = new TreeNode(1);
+    // root->left = new TreeNode(2);
+    // root->right = new TreeNode(2);
 
-    root->left->right = new TreeNode(3);
-    root->right->right = new TreeNode(3);
+    // root->left->right = new TreeNode(3);
+    // root->right->right = new TreeNode(3);
 
-    Solution sol;
-    sol.isSymmetric(root)?(cout << "yes" << endl):(cout << "false" << endl);
+    // Solution sol;
+    // sol.isSymmetric(root)?(cout << "true" << endl):(cout << "false" << endl);
 
-    RecursiveSolution rsol;
-    rsol.isSymmetric(root)?(cout << "yes" << endl):(cout << "false" << endl);
+    // RecursiveSolution rsol;
+    // rsol.isSymmetric(root)?(cout << "true" << endl):(cout << "false" << endl);
+
+    IterativeSolution isol;
+    isol.isSymmetric(root)?(cout << "true" << endl):(cout << "false" << endl);
 
     return 0;
 }
