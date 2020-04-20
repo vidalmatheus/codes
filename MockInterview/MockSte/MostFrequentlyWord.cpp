d1f59934a3ef2cfe379653900b8d4a60921ad247
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-04-20 02:39:47 -0300

    Mock Ste

---
+++ MockInterview/MockSte/MostFrequentlyWord.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static int speedUp=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
