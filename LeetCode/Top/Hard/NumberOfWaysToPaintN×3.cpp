e71882f48d95ad97472e4849be6b99768d1ba8db
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-04-21 02:29:21 -0300

    Number of Ways to Paint Nx3 Grid

--- LeetCode/Top/Hard/NumberOfWaysToPaintN×3.cpp
+++ LeetCode/Top/Hard/NumberOfWaysToPaintN×3.cpp
@@ -9,7 +9,63 @@ static int speedUp=[](){
     return 0;
 }();
 
-class Solution {
+class TopDowmSolution { // Top-Down Approach
+public:
+    int numOfWays(int n) {
+        return solve2Colors(n) + solve3Colors(n);
+    }
+
+    int solve2Colors(int n){
+        if (n==1)
+            return 6;
+        return 3*solve2Colors(n-1) + 2*solve3Colors(n-1);
+    }
+
+    int solve3Colors(int n){
+        if (n==1)
+            return 6;
+        return 2*solve2Colors(n-1) + 2*solve3Colors(n-1);
+    }
+};
+// Time: O(2^n)
+// Space: O(n)
+
+class TopDowmDPSolution { // Dynamic Programming (memoization) Top-Down Approach
+private:
+    vector<unsigned long int> memo2;
+    vector<unsigned long int> memo3;
+public:
+    int numOfWays(int n) {
+        memo2.resize(n+1,-1);
+        memo3.resize(n+1,-1);
+        
+        return (solve2Colors(n) + solve3Colors(n))%(1000000007);
+    }
+
+    unsigned long int solve2Colors(int n){
+        if (n==1)
+            return memo2[n] = 6;
+        
+        if (memo2[n]!=-1)
+            return memo2[n];
+
+        return memo2[n] = (3*solve2Colors(n-1) + 2*solve3Colors(n-1))%1000000007;
+    }
+
+    unsigned long int solve3Colors(int n){
+        if (n==1)
+            return memo3[n] = 6;
+        
+        if (memo3[n]!=-1)
+            return memo3[n];
+
+        return memo3[n] = (2*solve2Colors(n-1) + 2*solve3Colors(n-1))%1000000007;
+    }
+};
+// Time: O(n)
+// Space: O(n)
+
+class Solution { // Bottom-Up approach
 public:
     int numOfWays(int n) {
         unsigned long int twoColors = 6;
@@ -26,7 +82,7 @@ public:
         return (ansTwo + ansThree)%(1000000007);
     }
 };
-// Time: O(N)
+// Time: O(n)
 // Space: O(1)
 
 int main(){
