e851fd070a3a1382ed93a36958130a0aa74a379b
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-04-22 16:56:34 -0300

    Count Number Subarrays Sum Equals k

---
+++ LeetCode/Top/Medium/SubarraySumEqualsK.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+static int speedUp=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        if (nums.size()==0)
+            return 0;
+        
+        int n = nums.size();
+        vector<int> prefixSum(n+1,0);
+        for (int i=1;i<prefixSum.size();i++)
+            prefixSum[i] = nums[i-1] + prefixSum[i-1];
+        
+        unordered_map<int,int> freq;
+        int counter = 0;
+        for (int e:prefixSum){
+            if (freq.find(e-k)!=freq.end()) // found
+                counter += freq[e-k];
+            if (freq.find(e)!=freq.end()) // found
+                freq[e]++;
+            else freq[e]=1;
+        }
+    
+        return counter;        
+    }
+};
+// Time: O(n)
+// Space: O(n)
+
+int main(){
+    vector<int> nums = {1,1,1};
+    int k = 2;
+
+
+    Solution sol;
+    cout << sol.subarraySum(nums,k) << endl;
+
+    return 0;
+}
