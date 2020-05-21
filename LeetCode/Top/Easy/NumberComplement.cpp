baa1857a07ce316830e1c9c369fc0ef39653b6bb
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-05-21 10:42:50 -0300

    Number Complement

--- LeetCode/Top/Easy/NumberComplement.cpp
+++ LeetCode/Top/Easy/NumberComplement.cpp
@@ -15,8 +15,8 @@ public:
         int n = num==0 ? 1 : (int)log2(num)+1;
         for (int i=0;i<n;i++)
             if (num&(1<<i))
-                num &= (~(1<<i));
-            else num |= (1<<i);
+                num &= (~(1<<i)); // clear ith bit
+            else num |= (1<<i); // set ith bit
         return num;
     }
 };
