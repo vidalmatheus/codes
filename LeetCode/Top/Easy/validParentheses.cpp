9813dd78de4f84535c7770ad8f645f01c8fd4317
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-06-05 14:00:34 -0300

    Valid Parentheses

--- LeetCode/Top/Easy/validParentheses.cpp
+++ LeetCode/Top/Easy/validParentheses.cpp
@@ -9,7 +9,7 @@ static int speedUp=[](){
     return 0;
 }();
 
-class Solution {
+class FirstSolution {
 public:
     bool isValid(string s) {
         stack<char> st;
@@ -29,6 +29,34 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.size() == 0)
+            return true;
+        
+        stack<char> open;
+        for (int i=0;i<s.size();i++){
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+                open.push(s[i]);
+            else if (!open.empty()){
+                if (s[i] == ')' && open.top() == '(')
+                    open.pop();
+                else if (s[i] == ']' && open.top() == '[')
+                    open.pop();
+                else if (s[i] == '}' && open.top() == '{')
+                    open.pop();
+                else return false;
+            }
+            else return false;
+        }
+        
+        return open.empty() ? true : false;
+    }
+};
+// Time: O(n)
+// Space: O(n)
 
 int main(){
     string s = "(])";
