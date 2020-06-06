5f513d24b45d7cda35e932390292eb47879fc91f
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-06-06 20:21:52 -0300

    MInimum Remove Parentheses

--- LeetCode/Top/Medium/minimumRemoveParentheses.cpp
+++ LeetCode/Top/Medium/minimumRemoveParentheses.cpp
@@ -59,10 +59,12 @@ public:
     string minRemoveToMakeValid(string s) {
         stack<int> st;
         for (auto i = 0; i < s.size(); ++i) {
-            if (s[i] == '(') st.push(i);
+            if (s[i] == '(') 
+                st.push(i);
             if (s[i] == ')') {
-            if (!st.empty()) st.pop();
-            else s[i] = '*';
+                if (!st.empty()) 
+                    st.pop();
+                else s[i] = '*';
             }
         }
             
