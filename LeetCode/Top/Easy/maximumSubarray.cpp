729afb47ebd0939c2f9f76a3d3de8000b40e1d8b
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-06-18 00:47:46 -0300

    Maximum Subarray

--- LeetCode/Top/Easy/maximumSubarray.cpp
+++ LeetCode/Top/Easy/maximumSubarray.cpp
@@ -9,7 +9,7 @@ auto speedup=[](){
     return nullptr;
 }();
 
-class Solution { // Kadane's Algorithm
+class KadaneSolution { // Kadane's Algorithm
 public:
     int maxSubArray(vector<int>& nums) {
         int prev = nums[0];
@@ -22,6 +22,57 @@ public:
         }
         
         return maxSum;
+    }
+};
+// Time: O(n)
+// Space: O(1)
+
+//   [ -2, 1,-3,4,-1,2,1,-5,4]
+//  0  -2 -1 -4 0 -1 1 3 -2 2
+
+class Solution {
+public:
+    int maxSubArray (vector<int>& nums){
+        if (nums.size() == 0)
+            return 0;
+        
+        int n = nums.size();
+        vector<int> prefixSum(nums.size()+1,0);
+        for (int i=1;i<=n;i++)
+            prefixSum[i] = prefixSum[i-1]+nums[i-1];
+
+        int maxSubarraySum = INT_MIN;
+        int minValue = 0;
+        for (int i=1;i<=n;i++){
+            minValue = min(minValue,prefixSum[i-1]);
+            maxSubarraySum = max(maxSubarraySum, prefixSum[i] - minValue);
+        }
+        
+        return maxSubarraySum;
+    }
+};
+// Time: O(n)
+// Space: O(n)
+
+class BestSolution {
+public:
+    int maxSubArray (vector<int>& nums){
+        if (nums.size() == 0)
+            return 0;
+        
+        int maxSubarraySum = INT_MIN;
+        int minValue = 0;
+        
+        int prevPrefixSum = 0;
+        int currPrefixSum = 0;
+        for (int i:nums){
+            currPrefixSum += i;
+            minValue = min(minValue,prevPrefixSum);
+            maxSubarraySum = max(maxSubarraySum, currPrefixSum - minValue);
+            prevPrefixSum = currPrefixSum;
+        }
+        
+        return maxSubarraySum;
     }
 };
 // Time: O(n)
