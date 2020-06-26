30b8b1660ca71ecdda17656f0e5763fb31f2caeb
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-06-26 00:01:27 -0300

    Longest Duplicate Substring (KMP Solution - TLE)

--- LeetCode/Top/Medium/longestDuplicateSubstring.cpp
+++ LeetCode/Top/Medium/longestDuplicateSubstring.cpp
@@ -70,6 +70,99 @@ public:
 // Time: O(n*log n), on average
 // Space: O(1)
 
+class KMPSolution { // This solution was worth to understand that essentially KMP looks for non-overlapping substrings.
+public:
+    string longestDupSubstring(string S) {
+        
+        int size = S.size();
+        int max_occurrences = INT_MIN;
+        int suffix_size = 0;
+        int last_occurrence = -1;
+        for (int i=1;i<size;i++){
+            
+            string suffix = S.substr(i,size-i);
+            cout << suffix << endl;
+            pair<int,int> occurrences = kmp(S,suffix); // {last_occurrence, amount_of_occurrences}
+            
+            if (occurrences.second > max_occurrences){
+                max_occurrences = occurrences.second;
+                suffix_size = size-i;
+                last_occurrence = occurrences.first;
+            }
+        }
+        if (max_occurrences < 2)
+            return "";
+
+        return S.substr(last_occurrence,suffix_size);
+    }
+    
+    // abcd
+    //     i
+    // bcd
+    //    j
+    
+    pair<int,int> kmp (string& s, string& p){
+        if (s.size() == 0 || p.size() > s.size())
+            return {-1,0};
+        
+        vector<int> F;
+        getFailureFunction(p,F);
+        
+        int counter = 0;
+        
+        int end_pattern = -1;
+        
+        int i = 0; // goes through the text
+        int j = 0; // goes through the pattern
+        while (i < s.size()){
+            if (s[i] == p[j]){ // match
+                i++;
+                j++;
+                if (j == p.size()){
+                    counter++;
+                    if (counter > p.size()){
+                        counter = 1;
+                        break;
+                    }
+                    j = 0;
+                    end_pattern = --i;
+                }
+            }
+            else { // mismatch
+                if (j != 0) 
+                    j = F[j-1];
+                else i++;
+            }
+        }
+        
+        // end - begin + 1 = size
+        return {end_pattern-p.size()+1, counter};
+    }
+    
+    // i j
+    // aacdcd
+    // 010
+    //
+    void getFailureFunction(string& p, vector<int>& F){ // longest suffix that it is also a prefix
+        F.resize(p.size());
+        F[0] = 0;
+        int i = 0; // goes for the prefixes
+        int j = 0; // goes for the suffixes
+        while (j < p.size()){
+            if (p[j] == p[i]){
+                F[j++] = ++i;
+            }
+            else {
+                if (i != 0)
+                    i = F[i-1]; // you don't go all the way back!
+                else F[j++] = 0;
+            }
+        }
+    }
+};
+// Time: O(n^2)
+// Space: O(n)
+
 int main(){
     string S = "banana";
 
