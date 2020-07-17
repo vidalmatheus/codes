d12ab61c94b4237a65b0c5135f78e470cd9510fa
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-07-17 02:36:26 -0300

    Pow(x,n) - Review and Recursive Solution

--- LeetCode/Top/Medium/Pow(x,n).cpp
+++ LeetCode/Top/Medium/Pow(x,n).cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 static int speedUp=[](){
+    #define endl '\n'
     std::ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
@@ -23,6 +24,48 @@ public:
 };
 // Time: O(1)
 // Space: O(1)
+
+class OtherSolution {
+public:
+    double myPow(double x, int n) {
+        if (x == 0)
+            return 0;
+        
+        if (n == 0)
+            return 1;
+        
+        int sign = 1;
+        if (x < 0 && n % 2 == 1)
+            sign = -1;
+            
+        return sign*exp(n*log(abs(x)));
+    }
+};
+// Time: O(1)
+// Space: O(1)
+
+class RecursiveSolution {
+public:
+    double myPow(double x, int n) {
+        if (x == 0)
+            return 0;
+        
+        if (n == 0)
+            return 1;
+
+        double _sqrt = myPow(x,n/2);
+
+        double ans = _sqrt*_sqrt;
+        if (n < 0 && n % 2 != 0)
+            ans *= ans*(1/x);
+        else if (n % 2 != 0)
+            ans *= ans*(x);
+
+        return ans;
+    }
+};
+// Time: O(log n)
+// Space: O(log n)
 
 int main(){
     double x = 2.0;
