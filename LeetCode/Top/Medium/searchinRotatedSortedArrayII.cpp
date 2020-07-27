fe249a1b9eab914b41fd8e4510c0cb90efa11c92
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-07-27 15:02:48 -0300

    Search in Rotated Sorted Array II

--- LeetCode/Top/Medium/searchinRotatedSortedArrayII.cpp
+++ LeetCode/Top/Medium/searchinRotatedSortedArrayII.cpp
@@ -31,14 +31,16 @@ public:
         return binarySearch(nums,left,right,target);
     }
     
-    int findPivot(const vector<int>& nums, int left, int right){
-        while(left<right){
-            int mid = left + (right-left)/2;
-            if (nums[left]<nums[mid])
-                left = mid;
-            else if (nums[left] == nums[mid] && left < mid) // here
+    int findPivot(const vector<int>& nums, int left, int right){            
+        while(left < right){
+            int mid=left+(right-left)/2;
+            if (nums[left] == nums[left+1])
                 left++;
-            else right = mid;
+            else if (nums[right-1] == nums[right])
+                right--;
+            else if(nums[mid]>nums[right])
+                left=mid+1;
+            else right=mid;
         }
         
         return left;
