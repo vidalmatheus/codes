973d417d622be61167d50c510792b89bd31ca470
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-08-04 01:17:02 -0300

    N-ary Tree Postorder Traversal

---
+++ LeetCode/Top/Easy/naryTreePostorderTraversal.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+static int speedUp=[](){
+    # define endl '\n'
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+class RecursiveSolution {
+public:
+    vector<int> postorder(Node* root) {
+        if (root == nullptr)
+            return {};
+        
+        vector<int> ans;
+        dfs(root,ans);
+        
+        return ans;
+    }
+    
+    void dfs(Node* root, vector<int>& ans){
+        if (root == nullptr)
+            return;
+        
+        for (Node* child:root->children)
+            if (child != nullptr)
+                dfs(child,ans);
+        ans.push_back(root->val);
+    }
+};
+// Time: O(n)
+// Space: O(n)
+
+class Solution { // iteratively
+public:
+    vector<int> postorder(Node* root) {
+        if (root == nullptr)
+            return {};
+        
+        vector<int> ans;
+        stack<Node*> s;
+        s.push(root);
+        while (!s.empty()){
+            Node* node = s.top();
+            s.pop();
+            ans.push_back(node->val);
+            
+            for (Node* child:node->children)
+                if (child != nullptr)
+                    s.push(child);
+        }
+        
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+// Time: O(n)
+// Space: O(n)
