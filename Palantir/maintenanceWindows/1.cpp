1d569c75de1c72e44055ad48a8112fa3502c8874
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-08-31 10:25:52 -0300

    Palantir - Redo it

---
+++ Palantir/maintenanceWindows/1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+
+input:
+3
+Thu 5 - Thu 6
+Tue 4 - Tue 7
+Sun 9 - Sun 10
+
+output:
+2 // how many reach the largest maintenance window
+1 // the largest maintenance window in hours
+
+*/
---
+++ Palantir/maintenanceWindows/2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+Merge Intervals
+
+input:
+3
+Thu 3 - Thu 6
+Tue 4 - Thu 7
+Sun 9 - Sun 10
+
+output:
+Tue 4 - Thu 7
+Sun 9 - Sun 10
+
+*/
